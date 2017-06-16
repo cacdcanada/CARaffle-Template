f90982416cbd3a452d314d53c2f7773eb0e176dd
cacdcanada <hicham.jellab@ca.com>
2017-06-15 22:28:53 -0400

    Update PageRunner.php

--- PageRunner.php
+++ PageRunner.php
@@ -14,7 +14,7 @@ class PageRunner {
     private $TO_WIN = "ENTER OUR RAFFLE TO WIN!";
     private $WINNERS = "RAFFLE WINNERS";
     private $DEMO = "CDBU Modern Software Factory Canada Demo";
-    private $BUILD = "281";
+    private $BUILD = "1081";
 
     private $page;
 
